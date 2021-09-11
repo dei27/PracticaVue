8cc388ba05338113b692abc363063a728fd4b9da
dei27 <deivicb93@gmail.com>
2021-09-10 20:44:28 -0600

    Algunos cambios

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const app = Vue.createApp({
     // template: '<div>Esto es una simple prueba</div>
     data(){
         return{
-            title: 'Framework Vue',
-            edad:60
+            title: '"Sabor Gourmet"',
+            parrafo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sint nisi animi temporibus voluptate iure cupiditate impedit tempora suscipit error doloribus sapiente tenetur quidem exercitationem qui odio odit vitae quae voluptatum debitis laborum, voluptates officiis dolorum eius. Totam laboriosam nisi maiores consequatur possimus in voluptas repellendus consequuntur accusantium! Veritatis doloribus rem ea natus asperiores illum quo inventore iste beatae ipsam.'
         }
     }
 });
